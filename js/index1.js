64c89c0ec651b7545dcb6238581861d1305239a6
Бакир Базаркул <bakirbazarkul16@gmail.com>
2024-03-18 07:54:35 +0300

    закончил работу Владика сайта

--- js/index1.js
+++ js/index1.js
@@ -1,11 +1,16 @@
 const basketCount = document.querySelector('#cart')
 const basketModl = document.querySelector('#basketModal')
+const basketModlContent = document.querySelector('#basketModal-container')
 const mobilBurgerModal = document.getElementById('mobil-burger-modal')
 const burgerModalLiNav = document.querySelectorAll('.burger-modal-li')
 const mainUnitContainer = document.getElementById('main_unit-container')
 const productMainWrapp = document.getElementById('product__main_wrapp')
 const navItem = document.querySelectorAll('.nav-item')
 const productMain = document.getElementById('product__main')
+const productAddBasketBtn = document.getElementById('product__add-basket_btn')
+
+if (!localStorage.getItem('productData')) localStorage.setItem('productData', JSON.stringify([]))
+else basketCount.textContent = JSON.parse(localStorage.getItem('productData')).length
 
 const fetchData = async () => {
   try {
@@ -17,7 +22,6 @@ const fetchData = async () => {
     const sticksBlock = document.querySelector('#sticks')
     const HTMLTemplate = (idElement, data) => {
       for (let product of data) {
-        // console.log(product)
         idElement.innerHTML += ` <div class="swiper-slide" >
                 <div class="product-block-img-container">
                   <img
@@ -27,7 +31,7 @@ const fetchData = async () => {
                   />
                   <p class="product-title">${product.title}</p>
                 </div>
-                 <button class="product-btn"  onclick ="openProductDetails('${product.id}','${product.img}','${product.title}','${product.moreDetails}','${product.price}','${product.productOptionImage}')">Подробнее</button>
+                 <button class="product-btn"  onclick ="openProductDetails('${product.id}','${product.img}','${product.title}','${product.moreDetails}','${product.price}','${product.productOptionImage}','${product.typeOfProduct}')">Подробнее</button>
                 </div> `
       }
     }
@@ -130,45 +134,6 @@ const fetchData = async () => {
 }
 fetchData()
 
-// const productDetails = (id, img, title, moreDetails, price, productOptionImage) => {
-//   const nubID = Number(id)
-//   const pricedata = Number(price)
-//   let str = productOptionImage
-//   let arrimg = str.split(',')
-//   const productDatas = {
-//     productId: nubID,
-//     img: img,
-//     productOptionImage: arrimg,
-//     title: title,
-//     moreDetails: moreDetails,
-//     price: pricedata
-//   }
-//   localStorage.setItem('productDetails', JSON.stringify([productDatas]))
-// }
-
-// if (localStorage.getItem('productData')) {
-//   const productData = JSON.parse(localStorage.getItem('productData'))
-//   basketCount.textContent = productData.length
-// } else localStorage.setItem('productData', JSON.stringify([]))
-
-// const addPorductBasket = (id, title, img, moreDetails, price) => {
-//   const nubID = Number(id)
-
-//   const productDatas = {
-//     productId: nubID,
-//     title: title,
-//     img: img,
-//     moreDetails: moreDetails
-//   }
-//   let addProducts = JSON.parse(localStorage.getItem('productData')) || []
-//   const productExists = addProducts.some((data) => data.productId === nubID)
-//   if (!productExists) {
-//     addProducts.push(productDatas)
-//     localStorage.setItem('productData', JSON.stringify(addProducts))
-//     basketCount.textContent = addProducts.length
-//   }
-// }
-
 document.addEventListener('DOMContentLoaded ', function () {
   var downloadTrigger = document.getElementById('downloadTrigger')
 
@@ -208,7 +173,36 @@ const openForm = () => {
   if (mobilBurgerModal.style.display == 'block') mobilBurgerModal.style.display = 'none'
   document.getElementById('myForm').style.display = 'flex'
 }
-
+const basketProductHtml = (basketModlContent, storageProduct) => {
+  let summ = 0
+  storageProduct.forEach((count) => {
+    count.quantity > 1 ? (summ += count.price * count.quantity) : (summ += count.price)
+  })
+  basketModlContent.innerHTML = `<div class="basketModal-product-block-wrapp">
+                 ${storageProduct
+                   .map(
+                     (item, index) =>
+                       `<div class="basketModal-product-block">
+                       <div class="basketModal-img-container">
+                         <img src="${item.img}" />
+                         <div class="basketModal-title-container">
+                           <h3>${item.title} <p class ="basket__title_view">${item.typeoFProduct}</p></h3>
+                           <p>${item.quantity} штук</p>
+                         </div>
+                       </div>
+                       <img src="./images/icons/cross.png" alt="delet" class="basketModal-delet-product" onclick="deletProductStorage(${index})" />
+                     </div>`
+                   )
+                   .join('')}
+                </div >
+                <div class="basketModal-total-amount">
+                  <div class="basketModal-total-container">
+                    <div>Итог:</div>
+                    <div>${summ}<span> рублей.</span></div>
+                  </div>
+                  <button class="basketModla-btn" type="button" onclick="openForm()">Оставить заявку</button>
+                </div>`
+}
 const closeForm = () => (document.getElementById('myForm').style.display = 'none ')
 const openMobalBurger = () => {
   if (basketModl.style.display == 'block') basketModl.style.display = 'none'
@@ -218,8 +212,20 @@ const closeMobalBurger = () => (mobilBurgerModal.style.display = 'none')
 const openBasker = () => {
   basketModl.style.display = 'block'
   if (basketModl.style.display == 'block') mobilBurgerModal.style.display = 'none'
+  const basketProducts = JSON.parse(localStorage.getItem('productData'))
+  // let summ = 0
+  // basketProducts.forEach((count) => {
+  //   count.quantity > 1 ? (summ += count.price * count.quantity) : (summ += count.price)
+  // })
+  basketProductHtml(basketModlContent, basketProducts)
 }
 const closeBasker = () => (basketModl.style.display = 'none')
+const deletProductStorage = (id) => {
+  const ProductsStorage = JSON.parse(localStorage.getItem('productData'))
+  const newProductStorage = ProductsStorage.filter((elem, index) => index !== id)
+  localStorage.setItem('productData', JSON.stringify(newProductStorage))
+  basketProductHtml(basketModlContent, newProductStorage)
+}
 
 burgerModalLiNav.forEach((nav) => {
   nav.addEventListener('click', () => {
@@ -238,19 +244,18 @@ navItem.forEach((nav) => {
     }
   })
 })
-const openProductDetails = (id, img, title, details, price, productOptionImage) => {
+
+const openProductDetails = (id, img, title, details, price, productOptionImage, typeOfProduct) => {
   mainUnitContainer.style.display = 'none'
   productMainWrapp.style.display = 'flex'
   const productId = Number(id)
+  const arraytypeOfProduct = typeOfProduct.split(',')
   let neWproductOptionImage = productOptionImage.split(',')
-  console.log(neWproductOptionImage)
   productMain.innerHTML = `
          <div class="product__img_container">
           <img src="${img}" alt="product" class="product__img" />
           <div class="product__img_menu-container">
-            <img src="${neWproductOptionImage[0]}" alt="product" class="product__img_manu" />
-            <img src="${neWproductOptionImage[1]}" alt="product" class="product__img_manu" />
-            <img src="${neWproductOptionImage[2]}" alt="product" class="product__img_manu" />
+            ${neWproductOptionImage.map((item) => `<img src='${item}' alt='product' class='product__img_manu' />`)}
           </div>
           <div class="productMobal_add_price-container">
             <div class="product__price_container">${price}<span>Р</span></div>
@@ -263,20 +268,14 @@ const openProductDetails = (id, img, title, details, price, productOptionImage) 
             <div class="product__view-quantity_container">
               <div class="product__title_container">
                 <div class="product__title">Вид</div>
-                <select class="product__options">
-                  <option value="">13г</option>
-                  <option value="">10гр</option>
-                  <option value="">20гр</option>
+                <select class="product__options" id="product__options">
+                  ${arraytypeOfProduct.map((item) => `<option value="${item}">${item}</option>`)}
                 </select>
               </div>
-              <div class="product__view_title_container">
+              <div class="c_title_container">
                 <div class="product__title_container">
                   <div class="product__title">Кол-во</div>
-                  <select class="product__options">
-                    <option value="">5 коробок</option>
-                    <option value="">10 коробок</option>
-                    <option value="">20 коробок</option>
-                  </select>
+                <input class="product__options product_input" id ="product_input" type="number" value="1">
                 </div>
               </div>
             </div>
@@ -284,10 +283,23 @@ const openProductDetails = (id, img, title, details, price, productOptionImage) 
           <div class="product__description_container">${details}</div>
           <div class="product__price_contianer">
             <div class="product__price_container">${price}<span>Р</span></div>
-            <button type="button" class="product__add-basket_btn">Добавить в корзину</button>
+            <button type="button" class="product__add-basket_btn" id = "product__add-basket_btn"onclick="addbasketProductStorage('${img}','${title}','${price}')">Добавить в корзину</button>
           </div>
         </div>
   `
+}
+
+// window.addEventListener('storage', function (e) {
+//   console.log(e)
+// })
+
+const addbasketProductStorage = (img, title, price) => {
+  const typeoFProduct = document.getElementById('product__options').value
+  const quantity = Number(document.getElementById('product_input').value)
+  addnewProduct = JSON.parse(localStorage.getItem('productData'))
+  addnewProduct.push({ img: img, title: title, price: Number(price), typeoFProduct: typeoFProduct, quantity: quantity })
+  localStorage.setItem('productData', JSON.stringify(addnewProduct))
+  basketCount.textContent = JSON.parse(localStorage.getItem('productData')).length
 }
 
 function submitForm() {
@@ -385,45 +397,21 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 })
 
-// Открывает новую страницу с подробной информацией о товаре
-// const openProductDetails = () => window.open('product-details.html', '_blank')
-
-// слайдер банера
 const sliderImages = document.querySelectorAll('.imagenavigation')
 const sliderElement = document.querySelector('.slider')
 let currentSliden = 0
 
-// function changeSlide(direction) {
-//   currentSlide += direction
-//   if (currentSlide < 0) {
-//     currentSlide = sliderImages.length - 1
-//   } else if (currentSlide >= sliderImages.length) {
-//     currentSlide = 0
-//   }
-//   slider.style.transform = `translateX(-${currentSlide * sliderImages[0].clientWidth}px)`
-// }
-// document.querySelector('.prev-btn').addEventListener('click', () => {
-//   changeSlide(-1)
-// })
-// document.querySelector('.next-btn').addEventListener('click', () => {
-//   changeSlide(1)
-// })
-
 function changeSlide(direction) {
   if (direction === -1) {
-    // User clicked "previous " button
     if (currentSlide === 0) {
-      // If it's the first slide, return and don't change the slide
       return
     }
-    currentSlide-- // Decrease the index
+    currentSlide--
   } else {
-    // User clicked "next " button
     if (currentSlide === sliderImages.length - 1) {
-      // If it's the last slide, reset the index to 0
       currentSlide = 0
     } else {
-      currentSlide++ // Increase the index
+      currentSlide++
     }
   }
   sliderElement.style.transform = `translateX(-${currentSlide * sliderImages[0].clientWidth}px)`
@@ -453,203 +441,3 @@ function toggleMenu() {
   var burgerMenu = document.querySelector('.burger-menu')
   burgerMenu.classList.toggle('active')
 }
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   const burgerMenu = document.querySelector(".burger-menu");
-//   const navbar = document.querySelector(".navbar");
-
-//   // Обработчик клика на бургер-меню
-//   burgerMenu.addEventListener("click", function () {
-//     navbar.classList.toggle("show-nav");
-//   });
-// });
-
-// function toggleText(elementId) {
-//   var element = document.getElementById(elementId);
-//   if (element.style.display === "none") {
-//     element.style.display = "block";
-//     // Скрываем кнопку "Читать полностью"
-//     var trigger = document.querySelector(".read-more-trigger");
-//     trigger.style.display = "none";
-//     // Можно добавить здесь другие действия при раскрытии текста
-//   } else {
-//     element.style.display = "none";
-//     // Можно добавить здесь другие действия при скрытии текста
-//   }
-// }
-// document.addEventListener("DOMContentLoaded", function () {
-//   const slider = document.querySelector(".product-slider");
-//   const slides = slider.querySelectorAll(".product-block");
-//   let currentIndex = 0;
-
-//   function moveSlide(direction) {
-//     currentIndex += direction;
-
-//     if (currentIndex >= slides.length) {
-//       currentIndex = 0; // Возвращаемся к первому слайду
-//     } else if (currentIndex < 0) {
-//       currentIndex = slides.length - 1; // Переходим к последнему слайду
-//     }
-
-//     let totalOffset = 0;
-//     for (let i = 0; i < currentIndex; i++) {
-//       totalOffset += slides[i].offsetWidth; // Учитываем ширину каждого предыдущего слайда
-//     }
-
-//     let offset;
-//     // Задаем определенные значения смещения для каждого слайда
-//     if (currentIndex === 0) {
-//       offset = -100; // Смещение для первого слайда
-//     } else if (currentIndex === 1) {
-//       offset = -490; // Смещение для второго слайда
-//     } else if (currentIndex === 2) {
-//       offset = -880; // Смещение для третьего слайда
-//     } else if (currentIndex === 3) {
-//       offset = -1270; // Смещение для четвертого слайда
-//     } else if (currentIndex === 4) {
-//       offset = -1660; // Смещение для пятого слайда
-//     } else if (currentIndex === 5) {
-//       offset = -2050; // Смещение для шестого слайда
-//     }
-
-//     slider.style.transition = "transform 0.7s ease";
-//     slider.style.transform = `translateX(${offset}px)`;
-//   }
-
-//   // Обработка клика на кнопку
-//   const nextslides = document.querySelector(".nextslides");
-//   nextslides.addEventListener("click", () => moveSlide(1));
-// });
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   const slider2 = document.getElementById("product-slider-2");
-//   const slides2 = slider2.querySelectorAll(".product-block");
-//   let currentIndex2 = 0;
-
-//   function moveSlide(direction) {
-//     currentIndex2 += direction;
-
-//     if (currentIndex2 >= slides2.length) {
-//       currentIndex2 = 0; // Возвращаемся к первому слайду
-//     } else if (currentIndex2 < 0) {
-//       currentIndex2 = slides2.length - 1; // Переходим к последнему слайду
-//     }
-
-//     let totalOffset = 0;
-//     for (let i = 0; i < currentIndex2; i++) {
-//       totalOffset += slides2[i].offsetWidth; // Учитываем ширину каждого предыдущего слайда
-//     }
-
-//     let offset;
-//     // Задаем определенные значения смещения для каждого слайда
-//     if (currentIndex2 === 0) {
-//       offset = -100; // Смещение для первого слайда
-//     } else if (currentIndex2 === 1) {
-//       offset = -490; // Смещение для второго слайда
-//     } else if (currentIndex2 === 2) {
-//       offset = -880; // Смещение для третьего слайда
-//     } else if (currentIndex2 === 3) {
-//       offset = -1270; // Смещение для четвертого слайда
-//     } else if (currentIndex2 === 4) {
-//       offset = -1660; // Смещение для пятого слайда
-//     } else if (currentIndex2 === 5) {
-//       offset = -2050; // Смещение для шестого слайда
-//     }
-
-//     slider2.style.transition = "transform 0.7s ease";
-//     slider2.style.transform = `translateX(${offset}px)`;
-//   }
-
-//   // Обработка клика на кнопку только для второй секции
-//   const nextslides2 = document.querySelector(".nextslides1");
-//   nextslides2.addEventListener("click", () => moveSlide(1));
-// });
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   const slider3 = document.getElementById("product-slider-3");
-//   const slides3 = slider3.querySelectorAll(".product-block");
-//   let currentIndex3 = 0;
-
-//   function moveSlide(direction) {
-//     currentIndex3 += direction;
-
-//     if (currentIndex3 >= slides3.length) {
-//       currentIndex3 = 0; // Возвращаемся к первому слайду
-//     } else if (currentIndex3 < 0) {
-//       currentIndex3 = slides3.length - 1; // Переходим к последнему слайду
-//     }
-
-//     let totalOffset = 0;
-//     for (let i = 0; i < currentIndex3; i++) {
-//       totalOffset += slides3[i].offsetWidth; // Учитываем ширину каждого предыдущего слайда
-//     }
-
-//     let offset;
-//     // Задаем определенные значения смещения для каждого слайда
-//     if (currentIndex3 === 0) {
-//       offset = -100; // Смещение для первого слайда
-//     } else if (currentIndex3 === 1) {
-//       offset = -490; // Смещение для второго слайда
-//     } else if (currentIndex3 === 2) {
-//       offset = -880; // Смещение для третьего слайда
-//     } else if (currentIndex3 === 3) {
-//       offset = -1270; // Смещение для четвертого слайда
-//     } else if (currentIndex3 === 4) {
-//       offset = -1660; // Смещение для пятого слайда
-//     } else if (currentIndex3 === 5) {
-//       offset = -2050; // Смещение для шестого слайда
-//     }
-
-//     slider3.style.transition = "transform 0.7s ease";
-//     slider3.style.transform = `translateX(${offset}px)`;
-//   }
-
-//   // Обработка клика на кнопку только для третьей секции
-//   const nextslides3 = document.querySelector(".nextslides2");
-//   nextslides3.addEventListener("click", () => moveSlide(1));
-// });
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   const slider4 = document.getElementById("product-slider-4");
-//   const slides4 = slider4.querySelectorAll(".product-block");
-//   let currentIndex4 = 0;
-
-//   function moveSlide(direction) {
-//     currentIndex4 += direction;
-
-//     if (currentIndex4 >= slides4.length) {
-//       currentIndex4 = 0; // Возвращаемся к первому слайду
-//     } else if (currentIndex4 < 0) {
-//       currentIndex4 = slides4.length - 1; // Переходим к последнему слайду
-//     }
-
-//     let totalOffset = 0;
-//     for (let i = 0; i < currentIndex4; i++) {
-//       totalOffset += slides4[i].offsetWidth; // Учитываем ширину каждого предыдущего слайда
-//     }
-
-//     let offset;
-//     // Задаем определенные значения смещения для каждого слайда
-//     if (currentIndex4 === 0) {
-//       offset = -100; // Смещение для первого слайда
-//     } else if (currentIndex4 === 1) {
-//       offset = -490; // Смещение для второго слайда
-//     } else if (currentIndex4 === 2) {
-//       offset = -880; // Смещение для третьего слайда
-//     } else if (currentIndex4 === 3) {
-//       offset = -1270; // Смещение для четвертого слайда
-//     } else if (currentIndex4 === 4) {
-//       offset = -1660; // Смещение для пятого слайда
-//     } else if (currentIndex4 === 5) {
-//       offset = -2050; // Смещение для шестого слайда
-//     }
-
-//     slider4.style.transition = "transform 0.7s ease";
-//     slider4.style.transform = `translateX(${offset}px)`;
-//   }
-
-//   // Обработка клика на кнопку только для третьей секции
-//   const nextslides4 = document.querySelector(".nextslides3");
-//   nextslides4.addEventListener("click", () => moveSlide(1));
-// });
-//
